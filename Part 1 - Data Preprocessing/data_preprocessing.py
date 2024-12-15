b0fb9c3059ad313cf937ac5cdf4fff95d4f08210
Dmytro Malkin <malkin.dmytro@gmail.com>
2024-12-15 12:52:08 +0100

    Multiple Linear Regression

--- Part 1 - Data Preprocessing/data_preprocessing.py
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 ## Importing the dataset
 
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv("Data.csv")
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
@@ -17,7 +17,8 @@ print(y)
 ## Taking care of missing data
 
 from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
 imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 
@@ -29,7 +30,10 @@ print(x)
 
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+
+ct = ColumnTransformer(
+    transformers=[("encoder", OneHotEncoder(), [0])], remainder="passthrough"
+)
 x = np.array(ct.fit_transform(x))
 
 print(x)
@@ -37,6 +41,7 @@ print(x)
 ### Encoding the Dependent Variable
 
 from sklearn.preprocessing import LabelEncoder
+
 le = LabelEncoder()
 y = le.fit_transform(y)
 
@@ -45,7 +50,8 @@ print(y)
 ## Splitting the dataset into the Training set and Test set
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
 
 print(x_train)
 
@@ -58,6 +64,7 @@ print(y_test)
 ## Feature Scaling
 
 from sklearn.preprocessing import StandardScaler
+
 sc = StandardScaler()
 x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
 x_test[:, 3:] = sc.transform(x_test[:, 3:])
