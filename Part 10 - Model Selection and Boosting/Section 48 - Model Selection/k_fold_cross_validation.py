6498df6b22a2b96b011ac6cb11327631ffb2a5a3
Dmytro Malkin <malkin.dmytro@gmail.com>
2025-01-20 11:43:37 +0100

    Part 10 - Model Selection and Boosting

--- Part 10 - Model Selection and Boosting/Section 48 - Model Selection/k_fold_cross_validation.py
+++ Part 10 - Model Selection and Boosting/Section 48 - Model Selection/k_fold_cross_validation.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 ## Importing the dataset
 
-dataset = pd.read_csv("Wine.csv")
+dataset = pd.read_csv("Social_Network_Ads.csv")
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
@@ -15,7 +15,9 @@ y = dataset.iloc[:, -1].values
 
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=0
+)
 
 ## Feature Scaling
 
@@ -25,19 +27,11 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-## Applying PCA
+## Training the Kernel SVM model on the Training set
 
-from sklearn.decomposition import PCA
+from sklearn.svm import SVC
 
-pca = PCA(n_components=2)
-X_train = pca.fit_transform(X_train)
-X_test = pca.transform(X_test)
-
-## Training the Logistic Regression model on the Training set
-
-from sklearn.linear_model import LogisticRegression
-
-classifier = LogisticRegression(random_state=0)
+classifier = SVC(kernel="rbf", random_state=0)
 classifier.fit(X_train, y_train)
 
 ## Making the Confusion Matrix
@@ -48,6 +42,14 @@ y_pred = classifier.predict(X_test)
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 accuracy_score(y_test, y_pred)
+
+## Applying k-Fold Cross Validation
+
+from sklearn.model_selection import cross_val_score
+
+accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10)
+print("Accuracy: {:.2f} %".format(accuracies.mean() * 100))
+print("Standard Deviation: {:.2f} %".format(accuracies.std() * 100))
 
 ## Visualising the Training set results
 
@@ -63,7 +65,7 @@ plt.contourf(
     X2,
     classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
     alpha=0.75,
-    cmap=ListedColormap(("red", "green", "blue")),
+    cmap=ListedColormap(("red", "green")),
 )
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
@@ -71,12 +73,12 @@ for i, j in enumerate(np.unique(y_set)):
     plt.scatter(
         X_set[y_set == j, 0],
         X_set[y_set == j, 1],
-        c=ListedColormap(("red", "green", "blue"))(i),
+        c=ListedColormap(("red", "green"))(i),
         label=j,
     )
-plt.title("Logistic Regression (Training set)")
-plt.xlabel("PC1")
-plt.ylabel("PC2")
+plt.title("Kernel SVM (Training set)")
+plt.xlabel("Age")
+plt.ylabel("Estimated Salary")
 plt.legend()
 plt.show()
 
@@ -94,7 +96,7 @@ plt.contourf(
     X2,
     classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
     alpha=0.75,
-    cmap=ListedColormap(("red", "green", "blue")),
+    cmap=ListedColormap(("red", "green")),
 )
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
@@ -102,11 +104,11 @@ for i, j in enumerate(np.unique(y_set)):
     plt.scatter(
         X_set[y_set == j, 0],
         X_set[y_set == j, 1],
-        c=ListedColormap(("red", "green", "blue"))(i),
+        c=ListedColormap(("red", "green"))(i),
         label=j,
     )
-plt.title("Logistic Regression (Test set)")
-plt.xlabel("PC1")
-plt.ylabel("PC2")
+plt.title("Kernel SVM (Test set)")
+plt.xlabel("Age")
+plt.ylabel("Estimated Salary")
 plt.legend()
 plt.show()
